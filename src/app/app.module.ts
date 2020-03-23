3ce6cb528cae31582c6316b9ac9e3757c52580ed
Samuel-Abel <samuelsa5760@gmail.com>
2020-03-22 21:09:09 -0400

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepgComponent } from './homepg/homepg.component';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
@@ -13,72 +11,53 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-=======
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
-import { FindplaceComponent } from './findplace/findplace.component';
+// import { FindplaceComponent } from './findplace/findplace.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule} from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input'
-import { AgmCoreModule } from '@agm/core';
->>>>>>> origin/cwampler
-=======
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { AgmCoreModule } from '@agm/core';
 import {MatGridListModule} from '@angular/material/grid-list';
 import { FooterComponent } from './footer/footer.component';
 import { HmgridComponent } from './hmgrid/hmgrid.component';
 import {MatCardModule} from '@angular/material/card';
-import {MatIconModule} from '@angular/material/icon';
->>>>>>> origin/sam
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepgComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
-    MainNavComponent
-=======
+    MainNavComponent,
     LoginComponent,
-    FindplaceComponent
->>>>>>> origin/cwampler
-=======
+    // FindplaceComponent,
     FooterComponent,
     HmgridComponent
->>>>>>> origin/sam
   ],
 
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-<<<<<<< HEAD
-<<<<<<< HEAD
     LayoutModule,
     MatToolbarModule,
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
-=======
+    MatListModule,
     FormsModule,
     HttpClientModule,
     ReactiveFormsModule,
     MatFormFieldModule,
     MatSelectModule,
     MatInputModule,
-    AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyAVOiIQKnLWfWrYJVdjYyKHmnTEcg4K1_Q',
-      libraries: ['places']
-    })
->>>>>>> origin/cwampler
-=======
+    // AgmCoreModule.forRoot({
+    //   apiKey: 'AIzaSyAVOiIQKnLWfWrYJVdjYyKHmnTEcg4K1_Q',
+    //   libraries: ['places']
+    // }),
     MatGridListModule,
     MatCardModule,
     MatIconModule
->>>>>>> origin/sam
   ],
 
   providers: [],
--- src/app/findplace/findplace.component.spec.ts
+++ src/app/findplace/findplace.component.spec.ts
@@ -1,25 +1,25 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FindplaceComponent } from './findplace.component';
+// import { FindplaceComponent } from './findplace.component';
 
-describe('FindplaceComponent', () => {
-  let component: FindplaceComponent;
-  let fixture: ComponentFixture<FindplaceComponent>;
+// describe('FindplaceComponent', () => {
+//   let component: FindplaceComponent;
+//   let fixture: ComponentFixture<FindplaceComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ FindplaceComponent ]
-    })
-    .compileComponents();
-  }));
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ FindplaceComponent ]
+//     })
+//     .compileComponents();
+//   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(FindplaceComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(FindplaceComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
