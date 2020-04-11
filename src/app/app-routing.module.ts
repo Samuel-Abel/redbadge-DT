e2ce1c94a60e26da3c11c099715427431e54327b
Samuel-Abel <samuelsa5760@gmail.com>
2020-04-11 09:31:11 -0400

    heroku

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepgComponent } from './homepg/homepg.component';
  import { FindplaceComponent } from './findplace/findplace.component';
+import { ItinerariesComponent } from './itineraries/itineraries.component';
+import { UserItinerariesComponent } from './useritineraries/useritineraries.component';
 
 const routes: Routes = [
   {path:'',redirectTo: 'homepg', pathMatch:'full'},
   {path:'homepg', component: HomepgComponent},
   {path:'findplace', component: FindplaceComponent},
+  {path:'itineraries', component: ItinerariesComponent},
+  {path:'useritineraries', component: UserItinerariesComponent},
 ];
 
 @NgModule({
